81ba4d2f750627b7ff0eb73e866096c808ba8e0f
rickbump <61394203+rickbump@users.noreply.github.com>
2026-09-16 11:20:41 -0400

    Fix Google Analytics implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,21 @@
 import type { Metadata } from "next";
-import "./globals.css";
-import { Header } from "@/components/navigation/Header";
+import Script from "next/script";
+
 import { Footer } from "@/components/layout/Footer";
+import { Header } from "@/components/navigation/Header";
+
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: {
-    default: "Savartus",
+    default: "Savartus | Manage. Store. Preserve.",
     template: "%s | Savartus",
   },
   description:
-    "Savartus provides data lifecycle management, active archive, and optical object storage technologies.",
+    "Savartus provides Data Lifecycle Management, Active Archive, optical object storage, and enterprise storage technologies for managing, storing, and preserving information.",
 };
+
+const GOOGLE_ANALYTICS_ID = "G-5ZF3GGF439";
 
 export default function RootLayout({
   children,
@@ -21,8 +26,31 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Header />
+
         {children}
+
         <Footer />
+
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag(){
+              dataLayer.push(arguments);
+            }
+
+            gtag('js', new Date());
+
+            gtag('config', '${GOOGLE_ANALYTICS_ID}', {
+              send_page_view: true
+            });
+          `}
+        </Script>
       </body>
     </html>
   );
